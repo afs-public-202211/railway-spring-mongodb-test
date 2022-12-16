792d8e9a3c765e7cdf146e4525179bbcb6528700
Fengda Huang <h@phodal.com>
2022-12-16 16:26:46 +0800

    Update HelloController.java

--- src/main/java/com/rest/springbootemployee/controller/HelloController.java
+++ src/main/java/com/rest/springbootemployee/controller/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
     @GetMapping(path = "/{userName}")
     public String getAll(@PathVariable String userName) {
 
-        return "new changes - Hello:" + userName;
+        return "Hello:" + userName;
     }
 }
